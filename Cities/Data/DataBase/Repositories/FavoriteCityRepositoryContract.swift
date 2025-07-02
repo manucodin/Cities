d163d2c127c6502d3f0db101ffe08cf3351425d7
Manu Rodríguez <manurs93@gmail.com>
2025-07-02 12:55:56 +0200

    Adds mock repository for favorite cities
    Adds a mock implementation of the `FavoriteCityRepositoryContract` for testing purposes.
    Renames `MockHTTPClient` to `HTTPClientMock` for consistency.
    This allows for easier and more reliable testing of features that rely on favorite city data.
    Removes `Sendable` conformance from `FavoriteCityRepositoryContract` protocol.

--- Cities/Data/DataBase/Repositories/FavoriteCityRepositoryContract.swift
+++ Cities/Data/DataBase/Repositories/FavoriteCityRepositoryContract.swift
@@ -5,7 +5,7 @@
 //  Created by Manuel Rodríguez Sebastián on 2/7/25.
 //
 
-protocol FavoriteCityRepositoryContract: Sendable {
+protocol FavoriteCityRepositoryContract {
     func isFavorite(_ id: Int) async throws -> Bool
     func addFavorite(_ id: Int) async throws
     func deleteFavorite(_ id: Int) async throws
